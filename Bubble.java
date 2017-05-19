19035a3022f77d2aec195dfa2e2517e5f3002e19
Sasha Fomina <Sasha Fomina>
2017-05-19 11:01:43 -0400

    started Bubble

---
+++ Bubble.java
@@ -0,0 +1,24 @@
+import java.util.List;
+import java.util.Iterator;
+import java.util.ArrayList;
+
+
+public class Bubble {
+    private int _state;
+    private int _color;
+    private double _angle;
+    private ArrayList<Bubble> _neighbors;
+    private _dx;
+    private _dy;
+    private _xcor;
+    private _ycor;
+
+    public static final int  RED = 0;
+    public static final int BLUE = 1;
+    public static final int YELLOW = 2;
+    public static final int GREEN = 3;
+    public static final int PINK = 4;
+
+    private static final int SPEED = 7;
+    
+}
